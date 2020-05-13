c020939749a6c7d96ad69ac21c0782af2eac9a1c
Valentin Gonzalez E <valentin.gonzalez.e@gmail.com>
2020-05-13 16:43:34 -0500

    Agregue bloque del usuario admin01 en equipo panbigdata001

--- cookbooks/my_cookbook/recipes/panbigdata001.rb
+++ cookbooks/my_cookbook/recipes/panbigdata001.rb
@@ -8,3 +8,20 @@ node.default['authorization']['sudo']['passwordless'] = false
 node.default['authorization']['sudo']['groups'] = ['linux_admin', 'sudo', 'admin']
 #
 include_recipe 'sudo'
+#
+#users_manage 'panbigdata001' do
+#  action [:nothing]
+#  data_bag 'panbigdata001'
+#end
+#
+user 'admin01' do
+  shell '/bin/false'
+  action 'lock'
+end
+#
+user 'panitops' do
+  shell '/bin/bash'
+  action 'modify'
+end
+#
+include_recipe 'users'
