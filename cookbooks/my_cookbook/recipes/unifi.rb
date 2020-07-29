6b48948003564433d8d9658976f4956a311682e9
Valentin Gonzalez E <valentin.gonzalez.e@gmail.com>
2020-07-29 16:18:13 -0500

    Agregue equipo unifi, controlado ubiquiti de tiendas

---
+++ cookbooks/my_cookbook/recipes/unifi.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: my_cookbook
+# Recipe:: unifi
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+#
+node.default['openssh']['server']['permit_root_login'] = "no"
+node.default['openssh']['server']['password_authentication'] = "yes"
+node.default['openssh']['server']['allow_groups'] = "linux_admin"
+#
+include_recipe 'openssh'
+#
+#
+node.default['authorization']['sudo']['passwordless'] = false
+node.default['authorization']['sudo']['groups'] = ['linux_admin']
+#
+include_recipe 'sudo'
