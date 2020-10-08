4645af8a7e064eafedd7107a9c207f60cc245578
Valentin Gonzalez E <valentin.gonzalez.e@gmail.com>
2020-10-08 14:51:14 -0500

    Agregue el recipe palv0007.rb en my_cookbook

---
+++ cookbooks/my_cookbook/recipes/palv0007.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook:: my_cookbook
+# Recipe:: palv0007
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+#
+node.default['openssh']['server']['permit_root_login'] = "no"
+node.default['openssh']['server']['password_authentication'] = "yes"
+node.default['openssh']['server']['allow_groups'] = "linux_admin inswitch"
+#
+include_recipe 'openssh'
+#
+#
+node.default['authorization']['sudo']['passwordless'] = true
+node.default['authorization']['sudo']['groups'] = ['linux_admin', 'inswitch', 'jenkins']
+#
+include_recipe 'sudo'
+#
+users_manage 'inswitch' do
+  action [:create]
+  data_bag 'inswitch'
+end
+#
+include_recipe 'users'
